063030d105d3acbb7abdd730a9b515301de45fa4
daisydu1012 <daisydu1012@gmail.com>
2026-04-15 03:38:46 -0700

    Fix DB host for CI

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
+files=$(find data/*)
+
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time parallel ./load_denormalized.sh ::: data/*
+time echo "$files" | parallel ./load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time parallel 'python3 load_tweets.py --db postgresql://postgres:pass@pg_normalized:5432/postgres --inputs {} --print_every 10000' ::: data/*
+time echo "$files" | parallel python3 load_tweets.py \
+    --db "postgresql://postgres:pass@pg_normalized:5432/postgres" \
+    --inputs={}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time parallel 'python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@pg_normalized_batch:5432/postgres --inputs {}' ::: data/*
+time echo "$files" | parallel python3 -u load_tweets_batch.py \
+    --db "postgresql://postgres:pass@pg_normalized_batch:5432/postgres" \
+    --inputs={}
