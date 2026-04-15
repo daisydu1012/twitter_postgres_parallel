97dd0a798700dcdb9ff0ed6cdb0aeafbca5c8934
daisydu1012 <daisydu1012@gmail.com>
2026-04-15 02:20:49 -0700

    Fix pg_normalized_batch schema

--- services/pg_normalized_batch/schema.sql
+++ services/pg_normalized_batch/schema.sql
@@ -1,263 +1,114 @@
-cat > load_tweets_batch.py << 'ENDOFFILE'
-#!/usr/bin/python3
-import psycopg2
-import sqlalchemy
-import os
-import datetime
-import zipfile
-import io
-import json
+CREATE EXTENSION postgis;
 
-def remove_nulls(s):
-    if s is None:
-        return None
-    else:
-        return s.replace('\x00','\\x00')
+\set ON_ERROR_STOP on
 
-def batch(iterable, n=1):
-    l = len(iterable)
-    for ndx in range(0, l, n):
-        yield iterable[ndx:min(ndx + n, l)]
+BEGIN;
 
-def _bulk_insert_sql(table, rows):
-    if not rows:
-        raise ValueError('Must be at least one dictionary in the rows variable')
-    else:
-        keys = set(rows[0].keys())
-        for row in rows:
-            if set(row.keys()) != keys:
-                raise ValueError('All dictionaries must contain the same keys')
-    sql = (f'''
-    INSERT INTO {table}
-        ('''
-        +
-        ','.join(keys)
-        +
-        ''')
-        VALUES
-        '''
-        +
-        ','.join([ '('+','.join([f':{key}{i}' for key in keys])+')' for i in range(len(rows))])
-        +
-        '''
-        ON CONFLICT DO NOTHING
-        '''
-        )
-    binds = { key+str(i):value for i,row in enumerate(rows) for key,value in row.items() }
-    return (' '.join(sql.split()), binds)
+/*
+ * Users may be partially hydrated with only a name/screen_name 
+ * if they are first encountered during a quote/reply/mention 
+ * inside of a tweet someone else's tweet.
+ */
+CREATE TABLE users (
+    id_users BIGINT PRIMARY KEY,
+    created_at TIMESTAMPTZ,
+    updated_at TIMESTAMPTZ,
+    url TEXT, 
+    friends_count INTEGER,
+    listed_count INTEGER,
+    favourites_count INTEGER,
+    statuses_count INTEGER,
+    protected BOOLEAN,
+    verified BOOLEAN,
+    screen_name TEXT,
+    name TEXT,
+    location TEXT,
+    description TEXT,
+    withheld_in_countries VARCHAR(2)[]
+);
 
-def bulk_insert(connection, table, rows):
-    if len(rows)==0:
-        return
-    sql, binds = _bulk_insert_sql(table, rows)
-    res = connection.execute(sqlalchemy.sql.text(sql), binds)
+/*
+ * Tweets may be entered in hydrated or unhydrated form.
+ */
+CREATE TABLE tweets (
+    id_tweets BIGINT PRIMARY KEY,
+    id_users BIGINT,
+    created_at TIMESTAMPTZ,
+    in_reply_to_status_id BIGINT,
+    in_reply_to_user_id BIGINT,
+    quoted_status_id BIGINT,
+    retweet_count SMALLINT,
+    favorite_count SMALLINT,
+    quote_count SMALLINT,
+    withheld_copyright BOOLEAN,
+    withheld_in_countries VARCHAR(2)[],
+    source TEXT,
+    text TEXT,
+    country_code VARCHAR(2),
+    state_code VARCHAR(2),
+    lang TEXT,
+    place_name TEXT,
+    geo geometry
+);
 
-def insert_tweets(connection, tweets, batch_size=1000):
-    for i,tweet_batch in enumerate(batch(tweets, batch_size)):
-        print(datetime.datetime.now(),'insert_tweets i=',i)
-        _insert_tweets(connection, tweet_batch)
+CREATE INDEX tweets_index_geo ON tweets USING gist(geo);
+CREATE INDEX tweets_index_withheldincountries ON tweets USING gin(withheld_in_countries);
 
-def _insert_tweets(connection, input_tweets):
-    users = []
-    tweets = []
-    users_unhydrated_from_tweets = []
-    users_unhydrated_from_mentions = []
-    tweet_mentions = []
-    tweet_tags = []
-    tweet_media = []
-    tweet_urls = []
+CREATE TABLE tweet_urls (
+    id_tweets BIGINT,
+    url TEXT,
+    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+);
 
-    for tweet in input_tweets:
-        if tweet['user']['url'] is None:
-            user_url = None
-        else:
-            user_url = tweet['user']['url']
-        users.append({
-            'id_users':tweet['user']['id'],
-            'created_at':tweet['user']['created_at'],
-            'updated_at':tweet['created_at'],
-            'screen_name':remove_nulls(tweet['user']['screen_name']),
-            'name':remove_nulls(tweet['user']['name']),
-            'location':remove_nulls(tweet['user']['location']),
-            'url':user_url,
-            'description':remove_nulls(tweet['user']['description']),
-            'protected':tweet['user']['protected'],
-            'verified':tweet['user']['verified'],
-            'friends_count':tweet['user']['friends_count'],
-            'listed_count':tweet['user']['listed_count'],
-            'favourites_count':tweet['user']['favourites_count'],
-            'statuses_count':tweet['user']['statuses_count'],
-            'withheld_in_countries':tweet['user'].get('withheld_in_countries',None),
-            })
+CREATE TABLE tweet_mentions (
+    id_tweets BIGINT,
+    id_users BIGINT,
+    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+);
 
-        try:
-            geo_coords = tweet['geo']['coordinates']
-            geo_coords = str(tweet['geo']['coordinates'][0]) + ' ' + str(tweet['geo']['coordinates'][1])
-            geo_str = 'POINT'
-        except TypeError:
-            try:
-                geo_coords = '('
-                for i,poly in enumerate(tweet['place']['bounding_box']['coordinates']):
-                    if i>0:
-                        geo_coords+=','
-                    geo_coords+='('
-                    for j,point in enumerate(poly):
-                        geo_coords+= str(point[0]) + ' ' + str(point[1]) + ','
-                    geo_coords+= str(poly[0][0]) + ' ' + str(poly[0][1])
-                    geo_coords+=')'
-                geo_coords+=')'
-                geo_str = 'MULTIPOLYGON'
-            except KeyError:
-                if tweet['user']['geo_enabled']:
-                    geo_str = None
-                    geo_coords = None
+CREATE INDEX tweet_mentions_index ON tweet_mentions(id_users);
 
-        try:
-            text = tweet['extended_tweet']['full_text']
-        except:
-            text = tweet['text']
+CREATE TABLE tweet_tags (
+    id_tweets BIGINT,
+    tag TEXT,
+    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+);
 
-        try:
-            country_code = tweet['place']['country_code'].lower()
-        except TypeError:
-            country_code = None
+COMMENT ON TABLE tweet_tags IS 'This table links both hashtags and cashtags';
+CREATE INDEX IF NOT EXISTS tweet_tags_index ON tweet_tags(id_tweets);
 
-        if country_code == 'us':
-            state_code = tweet['place']['full_name'].split(',')[-1].strip().lower()
-            if len(state_code)>2:
-                state_code = None
-        else:
-            state_code = None
+CREATE TABLE tweet_media (
+    id_tweets BIGINT,
+    url TEXT,
+    type TEXT,
+    FOREIGN KEY (id_tweets) REFERENCES tweets(id_tweets) DEFERRABLE INITIALLY DEFERRED
+);
 
-        try:
-            place_name = tweet['place']['full_name']
-        except TypeError:
-            place_name = None
+/*
+ * Precomputes the total number of occurrences for each hashtag
+ */
+CREATE MATERIALIZED VIEW tweet_tags_total AS (
+    SELECT 
+        row_number() over (order by count(*) desc) AS row,
+        tag, 
+        count(*) AS total
+    FROM tweet_tags
+    GROUP BY tag
+    ORDER BY total DESC
+);
 
-        if tweet.get('in_reply_to_user_id',None) is not None:
-            users_unhydrated_from_tweets.append({
-                'id_users':tweet['in_reply_to_user_id'],
-                'screen_name':tweet['in_reply_to_screen_name'],
-                })
+/*
+ * Precomputes the number of hashtags that co-occur with each other
+ */
+CREATE MATERIALIZED VIEW tweet_tags_cooccurrence AS (
+    SELECT 
+        t1.tag AS tag1,
+        t2.tag AS tag2,
+        count(*) AS total
+    FROM tweet_tags t1
+    INNER JOIN tweet_tags t2 ON t1.id_tweets = t2.id_tweets
+    GROUP BY t1.tag, t2.tag
+    ORDER BY total DESC
+);
 
-        tweets.append({
-            'id_tweets':tweet['id'],
-            'id_users':tweet['user']['id'],
-            'created_at':tweet['created_at'],
-            'in_reply_to_status_id':tweet.get('in_reply_to_status_id',None),
-            'in_reply_to_user_id':tweet.get('in_reply_to_user_id',None),
-            'quoted_status_id':tweet.get('quoted_status_id',None),
-            'geo_coords':geo_coords,
-            'geo_str':geo_str,
-            'retweet_count':tweet.get('retweet_count',None),
-            'quote_count':tweet.get('quote_count',None),
-            'favorite_count':tweet.get('favorite_count',None),
-            'withheld_copyright':tweet.get('withheld_copyright',None),
-            'withheld_in_countries':tweet.get('withheld_in_countries',None),
-            'place_name':place_name,
-            'country_code':country_code,
-            'state_code':state_code,
-            'lang':tweet.get('lang'),
-            'text':remove_nulls(text),
-            'source':remove_nulls(tweet.get('source',None)),
-            })
-
-        try:
-            urls = tweet['extended_tweet']['entities']['urls']
-        except KeyError:
-            urls = tweet['entities']['urls']
-        for url in urls:
-            tweet_urls.append({
-                'id_tweets':tweet['id'],
-                'url':url['expanded_url'],
-                })
-
-        try:
-            mentions = tweet['extended_tweet']['entities']['user_mentions']
-        except KeyError:
-            mentions = tweet['entities']['user_mentions']
-        for mention in mentions:
-            users_unhydrated_from_mentions.append({
-                'id_users':mention['id'],
-                'name':remove_nulls(mention['name']),
-                'screen_name':remove_nulls(mention['screen_name']),
-                })
-            tweet_mentions.append({
-                'id_tweets':tweet['id'],
-                'id_users':mention['id']
-                })
-
-        try:
-            hashtags = tweet['extended_tweet']['entities']['hashtags']
-            cashtags = tweet['extended_tweet']['entities']['symbols']
-        except KeyError:
-            hashtags = tweet['entities']['hashtags']
-            cashtags = tweet['entities']['symbols']
-        tags = [ '#'+hashtag['text'] for hashtag in hashtags ] + [ '$'+cashtag['text'] for cashtag in cashtags ]
-        for tag in tags:
-            tweet_tags.append({
-                'id_tweets':tweet['id'],
-                'tag':remove_nulls(tag)
-                })
-
-        try:
-            media = tweet['extended_tweet']['extended_entities']['media']
-        except KeyError:
-            try:
-                media = tweet['extended_entities']['media']
-            except KeyError:
-                media = []
-        for medium in media:
-            tweet_media.append({
-                'id_tweets':tweet['id'],
-                'url':medium['media_url'],
-                'type':medium['type']
-                })
-
-    bulk_insert(connection, 'users', users)
-    bulk_insert(connection, 'users', users_unhydrated_from_tweets)
-    bulk_insert(connection, 'users', users_unhydrated_from_mentions)
-    bulk_insert(connection, 'tweet_mentions', tweet_mentions)
-    bulk_insert(connection, 'tweet_tags', tweet_tags)
-    bulk_insert(connection, 'tweet_media', tweet_media)
-    bulk_insert(connection, 'tweet_urls', tweet_urls)
-
-    sql = sqlalchemy.sql.text('''
-    INSERT INTO tweets
-        (id_tweets,id_users,created_at,in_reply_to_status_id,in_reply_to_user_id,quoted_status_id,geo,retweet_count,quote_count,favorite_count,withheld_copyright,withheld_in_countries,place_name,country_code,state_code,lang,text,source)
-        VALUES
-        '''
-        +
-        ','.join([f"(:id_tweets{i},:id_users{i},:created_at{i},:in_reply_to_status_id{i},:in_reply_to_user_id{i},:quoted_status_id{i},ST_GeomFromText(:geo_str{i} || '(' || :geo_coords{i} || ')'), :retweet_count{i},:quote_count{i},:favorite_count{i},:withheld_copyright{i},:withheld_in_countries{i},:place_name{i},:country_code{i},:state_code{i},:lang{i},:text{i},:source{i})" for i in range(len(tweets))])
-        +
-        '''
-        ON CONFLICT DO NOTHING
-        '''
-        )
-    res = connection.execute(sql, { key+str(i):value for i,tweet in enumerate(tweets) for key,value in tweet.items() })
-
-if __name__ == '__main__':
-    import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--db',required=True)
-    parser.add_argument('--inputs',nargs='+',required=True)
-    parser.add_argument('--batch_size',type=int,default=1000)
-    args = parser.parse_args()
-
-    engine = sqlalchemy.create_engine(args.db, connect_args={
-        'application_name': 'load_tweets_batch.py --inputs '+' '.join(args.inputs),
-        })
-    connection = engine.connect()
-
-    with connection.begin() as trans:
-        for filename in sorted(args.inputs, reverse=True):
-            with zipfile.ZipFile(filename, 'r') as archive:
-                print(datetime.datetime.now(),filename)
-                for subfilename in sorted(archive.namelist(), reverse=True):
-                    with io.TextIOWrapper(archive.open(subfilename)) as f:
-                        tweets = []
-                        for i,line in enumerate(f):
-                            tweet = json.loads(line)
-                            tweets.append(tweet)
-                        insert_tweets(connection,tweets,args.batch_size)
-ENDOFFILE
+COMMIT;
