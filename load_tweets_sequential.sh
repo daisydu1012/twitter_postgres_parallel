384794e291f3c61539b2df6f4ee4f5b0d28108c0
daisydu1012 <daisydu1012@gmail.com>
2026-04-15 03:32:14 -0700

    Fix sequential loader script

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -8,9 +8,18 @@ time sh load_denormalized.sh data/*
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time python3 load_tweets.py --db postgresql://postgres:pass@localhost:5582/postgres --inputs data/* --print_every 10000
+for f in data/*; do
+    python3 load_tweets.py \
+        --db postgresql://postgres:pass@localhost:5582/postgres \
+        --inputs "$f" \
+        --print_every 10000
+done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time python3 -u load_tweets_batch.py --db postgresql://postgres:pass@localhost:5583/postgres --inputs data/*
+for f in data/*; do
+    python3 -u load_tweets_batch.py \
+        --db postgresql://postgres:pass@localhost:5583/postgres \
+        --inputs "$f"
+done
