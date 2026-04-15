93fc4607d4d7551e438034a971fe405e1da1a669
daisydu1012 <daisydu1012@gmail.com>
2026-04-15 03:35:42 -0700

    Fix DB connection for CI

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-files=$(find data/*)
-
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
@@ -10,9 +8,9 @@ time parallel ./load_denormalized.sh ::: data/*
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time parallel 'python3 load_tweets.py --db postgresql://postgres:pass@localhost:5582/postgres --inputs {} --print_every 10000' ::: data/*
+time parallel 'python3 load_tweets.py --db postgresql://postgres:pass@pg_normalized:5432/postgres --inputs {} --print_every 10000' ::: data/*
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time parallel 'python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:5583/postgres --inputs {}' ::: data/*
+time parallel 'python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@pg_normalized_batch:5432/postgres --inputs {}' ::: data/*
