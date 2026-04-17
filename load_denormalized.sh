d15dc0d9372d36da78c47fd2487f9b95f7c55c7e
daisydu1012 <daisydu1012@gmail.com>
2026-04-16 23:06:29 -0700

    fix denormalized loader to process all input files

--- load_denormalized.sh
+++ load_denormalized.sh
@@ -1,1 +1,7 @@
-unzip -p "$1" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:5581/ -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+#!/bin/sh
+for file in "$@"; do
+  unzip -p "$file" \
+    | sed 's/\\u0000//g' \
+    | psql postgresql://postgres:pass@localhost:5581/ \
+        -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
+done
